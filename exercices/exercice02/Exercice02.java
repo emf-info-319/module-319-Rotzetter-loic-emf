37518defa6ca7a3ec07df1f424e2065a223d2eec
Rotzetter Loïc <loic.rotzetter@studentfr.ch>
2024-08-29 09:57:30 +0200

    test

--- exercices/exercice02/Exercice02.java
+++ exercices/exercice02/Exercice02.java
@@ -2,6 +2,8 @@ package exercices.exercice02;
 
 public class Exercice02 {
     public static void main(String[] args) {
+        // debut de mon code
         System.out.println("Bonjour tout le monde!");
+        System.out.println("ligne de code 2");
     }
 }
