dc4a81f77c90deaaa83284c6120cf04a47e13d58
Rotzetter Loïc <loic.rotzetter@studentfr.ch>
2024-08-29 11:43:51 +0200

    test

--- exercices/exercice03/TypesDeBase.java
+++ exercices/exercice03/TypesDeBase.java
@@ -7,7 +7,7 @@ public class TypesDeBase {
     public static void main(String[] args) {
         int age = 17;
         String name = "loic"; 
-        String ville = "Marly"
-        System.out.println("Bonjour, je m'appelle " + name + " et j'ai " + age + " ans. J'habite à " + ville + );
+        String ville = "marly";
+        System.out.println("Bonjour, je m'appelle " + name + " et j'ai " + age + " ans. J'habite à " +ville+ ".");
     }
 }
