5706b04ccd0cc666621321d6430b44e0244740cb
Loïc Rotzetter <loic.rotzetter@studentfr.ch>
2024-09-03 18:28:35 +0200

    test

---
+++ exercices/exercice03/TypesDeBase2.java
@@ -0,0 +1,14 @@
+package exercices.exercice03;
+
+public class TypesDeBase2 {
+    public static void main(String[] args) {
+        int age = 10;
+            String prenom = "Loïc";
+            String nom = "Rotzetter";
+            String ville = "Marly";
+            String pays = "Suisse";
+
+        System.out.println("bonjour, je m'appelle " + prenom + " " + nom + ", et j'habite à " + ville + " en " + pays + "." );
+    }
+    
+}
