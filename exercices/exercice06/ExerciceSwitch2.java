0dcb148ae4d94f4a7289710f6f55ee4d14cb271e
loïc rotzetter <loic.rotzetter@studentfr>
2024-09-05 21:59:48 +0200

    drst

--- exercices/exercice06/ExerciceSwitch2.java
+++ exercices/exercice06/ExerciceSwitch2.java
@@ -2,15 +2,22 @@ package exercices.exercice06;
 
 public class ExerciceSwitch2 {
     public static void main(String[] args) {
-        int age = 13;
-        if (age == 7) {
-            System.err.println("Poussin");
-        }if (age == 8 || age == 9) {
-            System.out.println("Pupille");
-        }if (age == 10 || age == 11) {
-            System.out.println("Minime");
-        }if (age < 7 || age > 11) {
-            System.out.println("Inconnu");
+        int age = 8;
+        switch (age) {
+            case 7:
+                System.out.println("Poussin");
+                break;
+            case 8:
+            case 9:
+                System.out.println("Pupille");
+                break;
+            case 10:
+            case 11:
+                System.out.println("Minime");
+                break;
+            default:
+                System.out.println("Inconnu");
+                break;
         }
     }
 }
