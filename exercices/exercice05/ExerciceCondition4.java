ef9a0e7f8c93381d02562067db6caef3b7574acf
Rotzetter Loïc <loic.rotzetter@studentfr.ch>
2024-09-05 11:01:44 +0200

    hsr

--- exercices/exercice05/ExerciceCondition4.java
+++ exercices/exercice05/ExerciceCondition4.java
@@ -2,16 +2,16 @@ package exercices.exercice05;
 
 public class ExerciceCondition4 {
     public static void main(String[] args) {
-        int temperature = 5;
+        int temperature = 35;
         if (temperature < -10) {
             System.out.println("Il fait très froid");
-        } else if ((temperature > -10 && temperature < 0)) {
+        } else if ((temperature >= -10 && temperature < 0)) {
             System.out.println("Il fait froid");
-        }else if ((temperature > 0 && temperature <25)){
+        }else if ((temperature >= 0 && temperature < 25)){
             System.out.println("Il fait normal");
-        }else if ((temperature > 25 && temperature < 35)){
+        }else if ((temperature >= 25 && temperature < 35)){
             System.out.println("Il fait chaud");
-        }else if (temperature>35){
+        }else if (temperature >= 35){
             System.err.println( "Il fait très chaud");
         }
         }
