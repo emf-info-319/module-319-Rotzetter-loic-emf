048f32f683ab939492e842c503cd5d3923d69427
Rotzetter Loïc <loic.rotzetter@studentfr.ch>
2024-09-05 09:34:28 +0200

    daw

--- exercices/exercice05/ExerciceCondition2.java
+++ exercices/exercice05/ExerciceCondition2.java
@@ -4,7 +4,7 @@ public class ExerciceCondition2 {
     public static void main(String[] args) {
         int valeurATester = 1;
         System.out.println(valeurATester);
-        if (valeurATester% 2 == 0) {
+        if (valeurATester/ 2 == 0) {
             System.out.println("la valeure est pair");
         } else 
         System.out.println("la valeur est impair");
