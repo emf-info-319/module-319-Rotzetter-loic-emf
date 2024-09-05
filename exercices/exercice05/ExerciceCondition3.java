b57f97ea22e84edbca3130c64968b77139eb91b5
Rotzetter Loïc <loic.rotzetter@studentfr.ch>
2024-09-05 10:54:04 +0200

    fhsd

--- exercices/exercice05/ExerciceCondition3.java
+++ exercices/exercice05/ExerciceCondition3.java
@@ -4,7 +4,6 @@ public class ExerciceCondition3 {
     public static void main(String[] args) {
         int valeur1 = -2;
         int valeur2 = 6;
-
         if ((valeur1 > 0 && valeur2 > 0 || valeur1 < 0 && valeur2 < 0)) {
             System.out.println("Le résultat est positif");
         } else {
