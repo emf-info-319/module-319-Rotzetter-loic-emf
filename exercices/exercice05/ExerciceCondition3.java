434888657485da99c85c5312b57846d65fd24d2e
Rotzetter Loïc <loic.rotzetter@studentfr.ch>
2024-09-05 09:45:20 +0200

    test

---
+++ exercices/exercice05/ExerciceCondition3.java
@@ -0,0 +1,13 @@
+package exercices.exercice05;
+
+public class ExerciceCondition3 {
+    public static void main(String[] args) {
+        int valeur1 = -2;
+        int valeur2 = 6;
+
+        if (valeur1 * valeur2 >0 ) {
+            System.out.println("Le résultat est positif");
+        } else 
+        System.out.println("Le résultat est négatif");
+    }
+}
