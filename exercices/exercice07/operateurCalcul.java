65e5141d14d3ff45a22fbbf116e35906b5534f5c
loïc rotzetter <loic.rotzetter@studentfr>
2024-09-09 21:30:29 +0200

    cc

--- exercices/exercice07/operateurCalcul.java
+++ exercices/exercice07/operateurCalcul.java
@@ -2,7 +2,7 @@ package exercices.exercice07;
 
 public class operateurCalcul {
     public static void main(String[] args) {
-        int age = 90;
+        int age = 14;
         int année = 2024;
         int annéedenaissance = année - age;
         System.out.println("Je suis né en " + annéedenaissance + ".");
@@ -10,11 +10,11 @@ public class operateurCalcul {
             System.out.println("Et je suis à l'école primaire.");
         } else if (age > 12 && age <= 16) {
             System.out.println("Et je suis au cycle d'orientation.");
-        } else if (age > 16) {
+        } else if (age > 16 && age < 65 ) {
             System.out.println("Et j'ai fini le cycle d'orientation.");
         } else if (age < 4) {
             System.out.println("Je n'ai pas encore commencé l'école.");
-        } else if (age >= 65) {
+        } else if (age >= 65 && age <= 114) {
             System.out.println("Et je suis à la retraite");
         } else if (age > 114) {
             System.out.println("Menteur, c'est pas possible. T'es pas l'homme le plus agé du monde.");
