00fb5056a9e50a6f6c51e65679f55d07387ccd95
loïc rotzetter <loic.rotzetter@studentfr>
2024-09-19 19:15:51 +0200

    DW

---
+++ devoirs/devoir03/devoir03.java
@@ -0,0 +1,24 @@
+package devoirs.devoir03;
+
+public class devoir03 {
+    public static void main(String[] args) {
+        int jour = 19;
+        int mois = 9;
+        int annee = 2024;
+        int date = 19.09.2024;
+        
+
+
+        // pour calculer le jours de l'année
+        int JourAn = calculerJourAn (jour, mois, annee);
+        System.out.println("Le " + jour + "/" + mois + "/" + annee + " est le jour " + JourAn + " de l'An.");
+
+
+
+
+        int jourdelan = 0;
+        for (int i = 0; i < JourAn; i++) {
+        }
+            System.out.println(jourdelan);
+    }
+}
