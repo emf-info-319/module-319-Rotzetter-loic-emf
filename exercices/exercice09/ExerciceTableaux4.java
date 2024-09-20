c36f9ed85e53d7afacfb736d7f93f7555c8df8f7
loïc rotzetter <loic.rotzetter@studentfr>
2024-09-20 12:41:18 +0200

    test

---
+++ exercices/exercice09/ExerciceTableaux4.java
@@ -0,0 +1,19 @@
+package exercices.exercice09;
+
+public class ExerciceTableaux4 {
+    public final static int Min = 1;
+    public final static int Max = 100;
+
+    public static void main(String[] args) {
+        int nombre = ( int ) ( Math.random() * ( Max - Min + 1 ) ) + Min;
+
+        int[] tab = new int[5];
+        for (int i = 0; i < tab.length; i++) {
+            nombre++;
+            System.out.println("La cellule " + i + " : " +nombre);
+        }
+        
+
+        
+    }
+}
