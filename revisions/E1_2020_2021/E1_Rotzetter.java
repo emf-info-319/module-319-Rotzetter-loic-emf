e693f958dbb804a2f1743e439d13cc80bf3a3359
loïc rotzetter <loic.rotzetter@studentfr>
2024-09-26 21:00:38 +0200

    feafea

---
+++ revisions/E1_2020_2021/E1_Rotzetter.java
@@ -0,0 +1,21 @@
+package revisions.E1_2020_2021;
+
+public class E1_Rotzetter {
+    public static final int LIBRE = 0;
+    public static final int OCCUPEE = 0;
+    public static final int TOUCHEE = 0;
+
+    public static final int TAILLE_GRILLE[] = new int[20];
+
+    public static final int NBRE_BATEAUX[] = new int[5];
+
+    public static int choisirIndexAleatoire(int min, int max) {
+        return (int) (Math.random() * (max - min + 1)) + min;
+
+
+        
+    public static void main(String[] args) {
+        
+    }
+
+}
