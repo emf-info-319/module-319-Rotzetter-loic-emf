9b1ec4b6c9469579afb19204fe9af16d0569190e
loïc rotzetter <loic.rotzetter@studentfr>
2024-09-26 22:04:31 +0200

    g r

---
+++ exercices/Suplémentaires_1/Multiplication.java
@@ -0,0 +1,9 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int nbre = 7;
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(nbre + " x " + i + " = " + (nbre * i));
+
+        } }   
+}
