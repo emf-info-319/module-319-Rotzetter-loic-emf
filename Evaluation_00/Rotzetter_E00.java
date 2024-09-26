6239ba88dabdbbb8f832a8cf65167356dc2c7389
Rotzetter Loïc <loic.rotzetter@studentfr.ch>
2024-09-26 09:52:50 +0200

    t

--- Evaluation_00/Rotzetter_E00.java
+++ Evaluation_00/Rotzetter_E00.java
@@ -3,19 +3,19 @@ package Evaluation_00;
 public class Rotzetter_E00 {
     final int nombre_pts_victoire = 3;
     public static void main(String[] args) {
-        int nomEquipe2 = "barcelone";
-        int nomEquipe1 = "madride";
+        String nomEquipe2 = "barcelone";
+        String nomEquipe1 = "madride";
         int nombrePointEquipe1 = 0;
         int nombrePointEquipe2 = 0;
         
         
         for (int i = 0; i < 10; i++) {
-
+            int scoreEquipe1 = ( int ) ( Math.random() * ( 10 - 1 + 1 ) ) + 1;
+            int scoreEquipe2 = ( int ) ( Math.random() * ( 10 - 1 + 1 ) ) + 1;
         }
 
-        int scoreEquipe1 = ( int ) ( Math.random() * ( 10 - 1 + 1 ) ) + 1;
-        int scoreEquipe2 = ( int ) ( Math.random() * ( 10 - 1 + 1 ) ) + 1;
-            System.out.println(nomEquipe1);
+        
+            System.out.println(nomEquipe1 +"joue contre"+nomEquipe2+" et le résultat est : " +scoreEquipe1+ " à " +scoreEquipe2+ ".");
 
 
     }
