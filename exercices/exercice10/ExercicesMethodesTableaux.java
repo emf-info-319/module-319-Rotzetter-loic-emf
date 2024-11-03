8ffc5f1ee55482102e61e5404d97159bfb9eb767
loïc rotzetter <loic.rotzetter@studentfr>
2024-11-03 21:22:39 +0100

    sddwwaa

--- exercices/exercice10/ExercicesMethodesTableaux.java
+++ exercices/exercice10/ExercicesMethodesTableaux.java
@@ -2,15 +2,27 @@ public class ExercicesMethodesTableaux {
     public static void main(String[] args) {
         tab(5);
         tab2(8);
+        remplirTableauAleatoire(10);
     }
-    public static int[] tab(int taille){
+
+    public static int[] tab(int taille) {
         int[] tabok = new int[taille];
         System.out.println("ok");
         return tabok;
-       
+
     }
-    public static void tab2(int aperol){
-        int[] tabvr = new int[] {aperol};
+
+    public static void tab2(int aperol) {
+        int[] tabvr = new int[] { aperol };
         System.out.println(tabvr[0]);
+    }
+}
+
+public static int remplirTableauAleatoire(int taille) {
+    int[] remplirTableauAleatoire = new int[taille];
+    for (int i = 0; i < remplirTableauAleatoire.length; i++) {
+        int nombre = ( int ) ( Math.random() * ( 10 - 1 + 1 ) ) + 1; 
+        System.out.println(remplirTableauAleatoire);
+        return taille;
     }
 }
