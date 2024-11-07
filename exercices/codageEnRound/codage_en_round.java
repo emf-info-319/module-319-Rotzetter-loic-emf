4755bc7029343d4a3820a2c02c6ca882756476f6
Rotzetter Loïc <loic.rotzetter@studentfr.ch>
2024-11-07 09:09:25 +0100

    zuk

--- exercices/codageEnRound/codage_en_round.java
+++ exercices/codageEnRound/codage_en_round.java
@@ -4,6 +4,7 @@ public class codage_en_round {
     double PI = 3.14;
 
     public static void main(String[] args) {
+
         int[] rayon = new int[10];
         for (int i = 0; i < rayon.length; i++) {
             rayon [i] = generenombre(1, 10);
@@ -16,10 +17,9 @@ public class codage_en_round {
             }
         }
     }
-
-}
-
  public static int generenombre(int min, int max){
     int generenombre = ( int ) ( Math.random() * ( max - min + 1 ) ) + min;
     return generenombre;
  }
+
+}
