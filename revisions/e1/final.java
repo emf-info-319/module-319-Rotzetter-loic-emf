3f02d86bb0c6d6b45343e7f142f2743cc2179b78
loïc rotzetter <loic.rotzetter@studentfr>
2024-12-04 19:10:07 +0100

    da

--- revisions/e1/final.java
+++
@@ -1,9 +0,0 @@
-package revisions.e1;
-
-public class final {
-    public static final String EQUIPES = "FC Richemont", "FC Central", "FC Schoenberg", "FC Beauregard", "Team AFF", "Etoile Sport";
-    public static final int MAX_GOAL = 10;
-    public static int[] pointsEquipes;
-
-    
-}
