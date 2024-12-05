3b2d47a42bc54d0493cc2297191871311c710836
Rotzetter-loic-emf <loic.rotzetter@studentfr.ch>
2024-12-05 18:23:13 +0100

    g

--- Form 2/JeuDeNim/rep.java
+++ Form 2/JeuDeNim/rep.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ROTZETTER_E2 {
+public class rep {
     public static final int NOMBRE_BATONNETS = 21;
     public static final int MIN = 1;
     public static final int MAX = 3;
@@ -11,14 +11,14 @@ public class ROTZETTER_E2 {
         for (int i = 0; i < batonnets.length; i++) {
             batonnets[i] = 1;
         }
-        System.out.println("Bienvenue dans le jeu de Nim !");
-        System.out.println(" Règles : Vous et l'IA retirez tour à tour entre 1 et 3 \r\n" + "bâtonnets.");
-        System.out.println("Le joueur qui retire le dernier bâtonnet perd.");
-        boolean tourJoueur = true;
+            System.out.println("Bienvenue dans le jeu de Nim !");
+            System.out.println(" Règles : Vous et l'IA retirez tour à tour entre 1 et 3 \r\n" + "bâtonnets.");
+            System.out.println("Le joueur qui retire le dernier bâtonnet perd.");
+            boolean tourJoueur = true;
 
-        while (nbBatonnetRestants(batonnets) > 0) {
-            afficherBatonnets(batonnets);
-            tourJoueur = !tourJoueur;
+            while (nbBatonnetRestants(batonnets) > 0) {
+                afficherBatonnets(batonnets);
+                tourJoueur = !tourJoueur;
             if (tourJoueur) {
                 int nbEnleve = demanderNombre(scanner);
                 batonnets = enleverBatonnets(batonnets, nbEnleve);
@@ -28,47 +28,45 @@ public class ROTZETTER_E2 {
                 batonnets = enleverBatonnets(batonnets, nbEnleveIA);
             }
         }
-
-        if (tourJoueur) {
-            System.out.println("Vous avez retiré le dernier bâtonnet. L'IA \r\n" + "gagne ! ");
-        } else {
-            System.out.println("L'IA a retiré le dernier bâtonnet. Vous gagnez !");
-        }
+            if (tourJoueur) {
+                System.out.println("Vous avez retiré le dernier bâtonnet. L'IA \r\n" + "gagne ! ");
+            } else {
+                System.out.println("L'IA a retiré le dernier bâtonnet. Vous gagnez !");
+            }
         scanner.close();
     }
 
-    public static void afficherBatonnets(int[] batonnets){
-        for (int i = 0; i < batonnets.length; i++) {
-            if (batonnets[i]==1) {
-            System.out.print("|");
-            }else{
-                System.out.print("-");
-            }
-        }
-        System.out.println("");
-    }
-
-    public static int nbBatonnetRestants(int[] batonnets) {
-        int compteur = 0;
+    public static void afficherBatonnets(int[] batonnets) {
         for (int i = 0; i < batonnets.length; i++) {
             if (batonnets[i] == 1) {
-                compteur++;
+                System.out.print("| ");
+            } else {
+                System.out.print("- ");
             }
         }
-        return compteur;
     }
 
-    public static int[] enleverBatonnets(int[] batonnets, int nbEnleve) {
-        int nbRestants = nbBatonnetRestants(batonnets);
+    public static int nbBatonnetRestants(int[] batonnet) {
+        int nbRestants = 0;
+        for (int i = 0; i < batonnet.length; i++) {
+            if (batonnet[i] == 1) {
+                nbRestants++;
+            }
+        }
+        return nbRestants;
+    }
+
+    public static int[] enleverBatonnets(int[] batonnet, int nbEnleve) {
+        int nbRestants = nbBatonnetRestants(batonnet);
         int nbEnleveMax = Math.min(nbEnleve, nbRestants);
-
-        for (int i = batonnets.length - 1; i >= 0 && nbEnleveMax > 0; i--) {
-            if (batonnets[i] == 1) {
-                batonnets[i] = 0;
+        
+        for (int i = batonnet.length -1; i >= 0 && nbEnleveMax > 0; i--) {
+            if (batonnet[i] == 1) {
+                batonnet[i] = 0;
                 nbEnleveMax--;
             }
         }
-        return batonnets;
+        return batonnet;
     }
 
     public static int demanderNombre(Scanner scanner) {
---
+++ Form 2/entrainement.java
@@ -0,0 +1,12 @@
+public class entrainement {
+    public static final int NOMBRE_BATONNETS = 21;
+    public static final int MIN = 1;
+    public static final int MAX = 3;    
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+    public static void afficherBatonnets(int[] batonnets){
+        
+    }
+}
